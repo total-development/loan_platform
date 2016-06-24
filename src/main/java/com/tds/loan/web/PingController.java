da28a4296d2b91bc2cfdc59840356656975420da
Dacheng Yang <dacheng@DachengdeMacBook-Pro.local>
2016-06-24 00:17:04 -0700

    Add log and lombok support

--- src/main/java/com/tds/loan/web/PingController.java
+++ src/main/java/com/tds/loan/web/PingController.java
@@ -1,13 +1,16 @@
 package com.tds.loan.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PingController {
 
     @RequestMapping("/ping")
     public String ping() {
+        log.info("Get a ping request");
         return "Greetings from TDS!";
     }
 }
