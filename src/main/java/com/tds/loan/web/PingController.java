f8e0ca12b66cae536bf59c98f813d0ada5e11b90
Dacheng Yang <dacheng@DachengdeMacBook-Pro.local>
2016-06-23 23:05:46 -0700

    Set up with springboot and gradle

---
+++ src/main/java/com/tds/loan/web/PingController.java
@@ -0,0 +1,13 @@
+package com.tds.loan.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return "Greetings from TDS!";
+    }
+}
